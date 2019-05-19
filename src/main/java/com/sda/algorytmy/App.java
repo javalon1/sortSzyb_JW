b17a82ed4100eaf5cf8809efe03b9361cb2ee5cc
Jacek.Woj <jacek.wojtuszek@gmail.com>
2019-05-19 15:29:21 +0100

    123

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,9 +1,21 @@
 package com.sda.algorytmy;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Arrays;
+
+public class App {
+
+    public static int suma(int[] tab) {
+        if (tab.length == 0) {
+            return 0;
+        }
+        int[] tab2 = new int[tab.length - 1];
+        for (int i = 0; i < tab.length - 1; i++) {
+            tab2[i] = tab[i + 1];
+        }
+        return tab[0] + suma(tab2);
+    }
+    public static void main(String[] args) {
+        int[] tab = {2, 4, 6, 2, 2};
+        System.out.println(suma(tab));
     }
 }
